f80d427cfe81c7613a951fdaf4e5316f4e9ea3f1
Won Chak Leung <chak8x8@gmail.com>
2021-02-14 18:37:55 +1100

    changed app bar color

--- lib/screens/accountscreen/createscreen.dart
+++ lib/screens/accountscreen/createscreen.dart
@@ -5,12 +5,14 @@ class CreateScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Create Account'),
+        title: Text('Create Account', style: TextStyle(color: Colors.black,),),
+        backgroundColor: Colors.white,
       ),
       body: Column(
         children: [
+          SizedBox(height: 40.0),
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.only(right: 20.0, left: 20.0, top: 8.0, bottom: 8.0,),
             child: TextFormField(
               decoration: InputDecoration(hintText: 'First Name',
                   border: OutlineInputBorder(
@@ -18,27 +20,58 @@ class CreateScreen extends StatelessWidget {
               ),
             ),
           ),
-          TextFormField(
-            decoration: const InputDecoration(hintText: 'Last Name'),
+          Padding(
+            padding: const EdgeInsets.only(right: 20.0, left: 20.0, top: 8.0, bottom: 8.0,),
+            child: TextFormField(
+              decoration: InputDecoration(hintText: 'Last Name',
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(width: 5.0),),
+              ),
+            ),
           ),
-          TextFormField(
-            decoration: const InputDecoration(hintText: 'Gender'),
+          Padding(
+            padding: const EdgeInsets.only(right: 20.0, left: 20.0, top: 8.0, bottom: 8.0,),
+            child: TextFormField(
+              decoration: InputDecoration(hintText: 'Gender',
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(width: 5.0),),
+              ),
+            ),
           ),
-          TextFormField(
-            decoration: const InputDecoration(hintText: 'Email'),
+          Padding(
+            padding: const EdgeInsets.only(right: 20.0, left: 20.0, top: 8.0, bottom: 8.0,),
+            child: TextFormField(
+              decoration: InputDecoration(hintText: 'Email',
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(width: 5.0),),
+              ),
+            ),
           ),
-          TextFormField(
-            decoration: const InputDecoration(hintText: 'Enter Password'),
+          Padding(
+            padding: const EdgeInsets.only(right: 20.0, left: 20.0, top: 8.0, bottom: 8.0,),
+            child: TextFormField(
+              decoration: InputDecoration(hintText: 'Enter Password',
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(width: 5.0),),
+              ),
+            ),
           ),
-          TextFormField(
-            decoration: const InputDecoration(hintText: 'Confirm Password'),
+          Padding(
+            padding: const EdgeInsets.only(right: 20.0, left: 20.0, top: 8.0, bottom: 8.0,),
+            child: TextFormField(
+              decoration: InputDecoration(hintText: 'Confirm Password',
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(width: 5.0),),
+              ),
+            ),
           ),
+          SizedBox(height: 20.0),
           ButtonTheme(
-            minWidth: 200.0,
-            height: 40.0,
+            minWidth: 290.0,
+            height: 60.0,
             child: RaisedButton(
               onPressed: (){},
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0)),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
               child: Text('Create Account', style: TextStyle(color: Colors.white),),
               color: Colors.black,
             ),
